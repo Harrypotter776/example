9c744fcccc566f3acb1007de8b169b2686730824
Harrypotter776 <81213994+Harrypotter776@users.noreply.github.com>
2021-03-25 16:56:34 +0530

    Update back-anchor.js

--- components/back-anchor/back-anchor.js
+++ components/back-anchor/back-anchor.js
@@ -1,6 +1,6 @@
 const template = `
     <a href="javascript:history.back()">
-        <img class="img-back" src="studio/assets/icons/arrow-back.svg" alt="go back" />
+        <img class="img-back" src="https://raw.githack.com/Harrypotter776/example/main/assets/icons/arrow-back.svg" alt="go back" />
     </a>`;
 
 class BackAnchor extends HTMLElement {
@@ -14,7 +14,7 @@ class BackAnchor extends HTMLElement {
     connectedCallback() {
         this.classList.add('primary-button');
         this.classList.add('back-anchor');
-        this.shadowRoot.querySelector('.img-back').src = `${this.getAttribute('assetsUrl')}/icons/arrow-back.svg`;
+        this.shadowRoot.querySelector('.img-back').src = `${this.getAttribute('assetsUrl')}https://raw.githack.com/Harrypotter776/example/main/assets/icons/arrow-back.svg`;
     }
 }
 
